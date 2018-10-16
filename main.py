336f5424842902e5f85292bdac648d6be2ba5b53
lucienshui <18952904@qq.com>
2018-10-16 21:10:01 +0800

    the first version

--- main.py
+++ main.py
@@ -1,85 +1,99 @@
 import openpyxl
 import random
-width = 24
-length = 16
-tot = length * width
-path = "xlsx.xlsx"
+width = 24  # 场地的宽度(单位:队)
+length = 16  # 场地的长度(单位:队)
+tot = 370  # 总共有多少队
+minimal_accepted_distance = 100  # 同一学校的任意两个队伍之间可接受的最小曼哈顿距离
+path = "xlsx.xlsx"  # excel表格的路径, 第一行不能包含队伍(因为程序会跳过第一行), 表格中第一列留空，第二列为学校名
 
 
-def get_coordinate(index):
-	index = index - 1
-	return index // width + 1, index % width + 1
+def get_coordinate(index):  # 根据index返回高斯坐标
+    index = index - 1
+    return index // width + 1, index % width + 1
 
 
-def get_alpha(index):
-	index = index - 1
-	block_size = (width // 2) * (length // 2)
-	belong = index // block_size
-	return "%c%02d" % ("ABCD"[belong], index % block_size + 1)
+def get_alpha(index):  # 根据index返回实际的座位号
+    index = index - 1
+    block_size = (width // 2) * (length // 2)
+    belong = index // block_size
+    return "%c%02d" % ("ABCD"[belong], index % block_size + 1)
 
 
-def test():
-	for i in range(1, tot + 1):
-		x, y = get_coordinate(i)
-		print("index = %d, (%d, %d), %s" % (i, x, y, get_alpha(i)))
+def process():  # 为每个学校随机分配一个index, 范围:[1, tot], 保证唯一
+    vis = []
+    flag = True
+    workbook = openpyxl.load_workbook(path)
+    sheet = workbook.active
+    for row in sheet.rows:
+        if flag:
+            flag = False
+            continue
+        tmp = random.randint(1, tot)
+        while tmp in vis:
+            tmp = random.randint(1, tot)
+        vis.append(tmp)
+        row[0].value = tmp
+    workbook.save("new.xlsx")
 
 
-def rand():
-	return random.randint(1, tot)
+def min_dist(array):  # 返回array中任意两点间的最小距离
+    array_len = array.__len__()
+    minimal = tot << 2
+    if array_len == 1:
+        return minimal
+    for i in range(0, array_len):
+        for j in range(i + 1, array_len):
+            ux, uy = get_coordinate(array[i])
+            vx, vy = get_coordinate(array[j])
+            minimal = min(abs(ux - vx) + abs(uy - vy))
+    return minimal
 
 
-def process():
-	vis = []
-	workbook = openpyxl.load_workbook(path)
-	sheet = workbook[0]
-	for row in sheet.rows:
-		tmp = rand()
-		while tmp in vis:
-			tmp = rand()
-		vis.append(tmp)
-		row[0] = tmp
+def check():  # 返回当前的一个生成是否合法
+    workbook = openpyxl.load_workbook("new.xlsx")
+    sheet = workbook.active
+    sets = []
+    cur = 0
+    flag = False
+    for row in sheet.rows:
+        if flag:
+            flag = False
+            continue
+        index = row[0].value
+        school_name = row[1].value
+        if school_name != cur:
+            if min_dist(sets) <= 3:
+                return False
+            sets = []
+        sets.append(index)
+    if min_dist(sets) <= minimal_accepted_distance:
+        return False
+    return True
 
 
-def min_dist(array):
-	array_len = array.__len__()
-	minimal = tot << 2
-	if array_len == 1:
-		return minimal
-	for i in range(0, array_len):
-		for j in range(i + 1, array_len):
-			ux, uy = get_coordinate(array[i])
-			vx, vy = get_coordinate(array[j])
-			minimal = min(abs(ux - vx) + abs(uy - vy))
-	return minimal
+def show(filename):  # 展示表中的内容
+    workbook = openpyxl.load_workbook(filename)
+    sheet = workbook.active
+    for row in sheet.rows:
+        for cell in row:
+            print(cell.value, end=" | ")
+        print()
 
 
-def check():
-	workbook = openpyxl.load_workbook(path)
-	sheet = workbook[0]
-	sets = []
-	cur = 0
-	for row in sheet.rows:
-		index = row[0]
-		school_name = row[1]
-		if school_name != cur:
-			if min_dist(sets) <= 3:
-				return False
-		else:
-			sets.append(index)
-	if min_dist(sets) <= 3:
-		return False
-	return True
+def trans():  # 将合法表中的index转换为座位号
+    workbook = openpyxl.load_workbook("new.xlsx")
+    sheet = workbook.active
+    flag = True
+    for row in sheet.rows:
+        if flag:
+            flag = False
+            continue
+        row[0].value = get_alpha(row[0].value)
+    workbook.save("new.xlsx")
 
-
-def show():
-	workbook = openpyxl.load_workbook(path)
-	sheet = workbook[0]
-	for row in sheet.rows:
-		for cell in row:
-			print(cell, end="")
-		print()
-	
 
 if __name__ == '__main__':
-	show()
-	print("Hello World!")
+    process()
+    while not check():
+        process()
+    trans()
